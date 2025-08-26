56b844de2e42c547b033d7ac8ac0d9dee9f6f95b
Temka <thxdeadshotxht@gmail.com>
2025-08-26 22:27:41 +0800

    add some feature

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import Toggle from '@/component/toggle';
+import { useTranslations } from 'next-intl';
 
 export default function Home() {
+    const t = useTranslations('home');
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -14,6 +16,7 @@ export default function Home() {
                     height={38}
                     priority
                 />
+                <h1>{t('title')}</h1>
                 <Toggle />
                 <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
                     <li className="mb-2 tracking-[-.01em]">
